4fe4488f7d776825a604a308bfd11a31b339abda
Jari Aalto <jari.aalto@cante.net>
2012-01-14 10:53:35 +0200

    markdown-mode-epackage-install.el: Fix require

--- epackage/markdown-mode-epackage-install.el
+++ epackage/markdown-mode-epackage-install.el
@@ -1,4 +1,4 @@
-(require 'markdown-mode-0loaddefs)
+(require 'markdown-mode-epackage-0loaddefs)
 ;; Ther eis no standard file extension.
 ;; See http://superuser.com/questions/249436/file-extension-for-markdown-files
 ;; We select only the most used: .md (Github) and .markdown (most descriptive)
